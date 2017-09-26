f546346bb54e09b6755b3ad9823ce225b1ad7399
Aaron Samuel <aasamuelj@gmail.com>
2017-09-26 08:47:55 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -56,6 +56,6 @@ epic_tragedy = {
 }
 
 def bonus
-  [:montague][:hero][:status] = "dead"
-  [:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 end
