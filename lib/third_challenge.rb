e1b43daa4d37bb0f13de47554b6ab8040b4f92cf
Aaron Samuel <aasamuelj@gmail.com>
2017-09-26 08:07:19 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,18 +1,1 @@
-def first_challenge
-  epic_tragedy = {
-    montague: {},
-    capulet: {}
-  }
-end
 
-def second_challenge
-  epic_tragedy = {
-    montague: {
-      patriarch: {},
-      matriarch: {},
-      hero: {},
-      hero_friends: {}
-    },
-    capulet: {}
-  }
-end
